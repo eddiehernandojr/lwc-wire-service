95eec4ae8f0fbda12f38b4b86585c5c5e6698342
eddiehernandojr <edzstrock@hotmail.com>
2021-12-10 19:56:32 -0500

    refactor code for readability purposes

--- force-app/main/default/lwc/wiredOpportunities/wiredOpportunities.js
+++ force-app/main/default/lwc/wiredOpportunities/wiredOpportunities.js
@@ -1,52 +1,3 @@
-// import { LightningElement, wire } from 'lwc';
-// import getOpportunityList from '@salesforce/apex/OpportunityController.getOpportunityList';
-
-// // const DELAY = 300;
-// const columns = [
-//     { label: 'Id', fieldName: 'Id'},
-//     { label: 'Name', fieldName: 'Name'},
-//     { label: 'Stage', fieldName: 'StageName'},
-//     { label: 'Close Date', fieldName: 'CloseDate'},
-//     { label: 'Amount', fieldName: 'Amount', type: 'currency' }
-// ];
-
-// export default class WiredOpportunities extends LightningElement {
-//     searchKey = '';
-//     columns = columns;
-
-//     @wire(getOpportunityList, { searchKey: '$searchKey' })
-//     opportunities;
-
-//     // handleKeyChange(event) {
-//     //     window.clearTimeout(this.delayTimeout);
-//     //     const searchKey = event.target.value;
-//     //     this.delayTimeout = setTimeout(() => {
-//     //         this.searchKey = searchKey;
-//     //     }, DELAY);
-//     // }
-
-//     handleChange(event) {
-//         this.searchKey = event.target.value;
-//     }
-
-//     // TODO2: check if we can retrieve these values using Apex
-//     get options() {
-//         return [
-//             { label: 'Prospecting', value: 'Prospecting' },
-//             { label: 'Qualification', value: 'Qualification' },
-//             { label: 'Needs Analysis', value: 'Needs Analysis' },
-//             { label: 'Value Proposition', value: 'Value Proposition' },
-//             { label: 'Id. Decision Makers', value: 'Id. Decision Makers' },
-//             { label: 'Perception', value: 'Perception Analysis' },
-//             { label: 'Proposal/Price Quote', value: 'Proposal/Price Quote' },
-//             { label: 'Negotiation', value: 'Negotiation/Review' },
-//             { label: 'Closed/Won', value: 'Closed Won' },
-//             { label: 'Closed/Lost', value: 'Closed Lost' },
-//             { label: 'All', value: '%' },
-//         ];
-//     }
-// }
-
 import { LightningElement, wire } from 'lwc';
 import getOpportunityList from '@salesforce/apex/OpportunityController.getOpportunityList';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
@@ -55,9 +6,9 @@ import OPPORTUNITY_OBJECT from '@salesforce/schema/Opportunity';
 import STAGE_FIELD from '@salesforce/schema/Opportunity.StageName';
 
 const columns = [
-    { label: 'Id', fieldName: 'Id'},
-    { label: 'Name', fieldName: 'Name'},
-    { label: 'Stage', fieldName: 'StageName'},
+    { label: 'Id', fieldName: 'Id' },
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Stage', fieldName: 'StageName' },
     { label: 'Close Date', fieldName: 'CloseDate'},
     { label: 'Amount', fieldName: 'Amount', type: 'currency' }
 ];
@@ -82,14 +33,10 @@ export default class WiredOpportunities extends LightningElement {
         recordTypeId: '$opportunityMetadata.data.defaultRecordTypeId',
         fieldApiName: STAGE_FIELD
     })
-    getMyPicklistValues({ error, data }) {
+    getCustomPicklistValues({ error, data }) {
+        // TODO2: include no row message
         if (data) {
-            const newPickLists = data.values.map(stage => {
-                const obj = {};
-                obj.label = stage.label;
-                obj.value = stage.value;
-                return obj;
-            });
+            const newPickLists = data.values.map(stage => ({ label: stage.label, value: stage.value }));
             this.options = newPickLists.concat(picklists);
             this.error = undefined;
         } else if (error){
@@ -102,5 +49,3 @@ export default class WiredOpportunities extends LightningElement {
         this.searchKey = event.target.value;
     }
 }
-
-
